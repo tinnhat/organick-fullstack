c5c4a9aa0114c1553ea927ebdc45bcf6ae8e91b4
tinnhat <tinnhat0412@gmail.com>
2024-03-01 23:13:30 +0700

    update ui admin using materialpro-nextjs-free

--- FE/Client/src/app/(admin)/admin/page.tsx
+++ FE/Client/src/app/(admin)/admin/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
-type Props = {}
-
-export default function Admin({}: Props) {
+'use client'
+import { Grid, Box } from '@mui/material';
+// components
+import SalesOverview from './components/dashboard/TheSalesOverview';
+import Blogcard from './components/dashboard/TheBlogCard';
+import ProfileCard from "./components/dashboard/TheProfileCard";
+import MyContacts from "./components/dashboard/TheMyContacts";
+import ActivityTimeline from "./components/dashboard/TheActivityTimeline";
+import PageContainer from './components/container/PageContainer';
+const Dashboard = () => {
   return (
-    <div>Admin</div>
+    <PageContainer title="Dashboard" description="this is Dashboard">
+      <Box mt={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={8}>
+            <SalesOverview />
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Blogcard />
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <ProfileCard />
+              </Grid>
+              <Grid item xs={12}>
+                <MyContacts />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={8}>
+            <ActivityTimeline />
+          </Grid>
+        </Grid>
+      </Box>
+    </PageContainer>
   )
 }
+
+export default Dashboard;
