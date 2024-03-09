41c324f07854a647b95a7cce3b8a3a43e5479de2
Nhat Tin <tinnhat0412@gmail.com>
2024-03-09 15:07:50 +0700

    update layout order detail

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './_scrollbar.scss'
-import ToastContainer from './components/toast'
 const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +16,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         {children}
-        <ToastContainer />
+       
       </body>
     </html>
   )
