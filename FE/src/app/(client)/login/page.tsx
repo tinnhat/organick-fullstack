01fff78551cc0dca7e9439690be51fcc04707a18
Nhat Tin <tinnhat0412@gmail.com>
2024-03-15 10:55:35 +0700

    integrate api login

--- FE/src/app/(client)/login/page.tsx
+++ FE/src/app/(client)/login/page.tsx
@@ -2,10 +2,12 @@
 import React, { useState } from 'react'
 import './style.scss'
 import Image from 'next/image'
+import LoginForm from './loginForm'
 type Props = {}
 
 export default function AuthPage({}: Props) {
   const [showRegister, setShowRegister] = useState(false)
+
   return (
     <div className='auth-page'>
       <div className='container'>
@@ -24,7 +26,7 @@ export default function AuthPage({}: Props) {
           {showRegister ? (
             <div className='box'>
               <p className='box__title'>Create your Account</p>
-              <form action=''>
+              <form >
                 <div className='input-box'>
                   <label htmlFor='fullname'>Full Name</label>
                   <br />
@@ -61,7 +63,13 @@ export default function AuthPage({}: Props) {
                 <div className='input-box'>
                   <label htmlFor='rePassword'>Re-Password</label>
                   <br />
-                  <input role='presentation' placeholder='Re-type your password' autoComplete='off' type='password' id='rePassword' />
+                  <input
+                    role='presentation'
+                    placeholder='Re-type your password'
+                    autoComplete='off'
+                    type='password'
+                    id='rePassword'
+                  />
                 </div>
                 <button className='btn-create-account'>Create Account</button>
               </form>
@@ -70,39 +78,7 @@ export default function AuthPage({}: Props) {
               </p>
             </div>
           ) : (
-            <div className='box'>
-              <p className='box__title mb-0 title'>Welcome to Organick</p>
-              <p className='box__title'>Login</p>
-              <form action=''>
-                <div className='input-box'>
-                  <label htmlFor='email'>Email</label>
-                  <br />
-                  <input
-                    role='presentation'
-                    autoComplete='off'
-                    type='email'
-                    id='email'
-                    placeholder='Enter your email'
-                  />
-                </div>
-                <div className='input-box'>
-                  <label htmlFor='password'>Password</label>
-                  <br />
-                  <input
-                    role='presentation'
-                    autoComplete='off'
-                    type='password'
-                    id='password'
-                    placeholder='Enter your password'
-                  />
-                </div>
-
-                <button className='btn-create-account'>Login</button>
-              </form>
-              <p className='box__text'>
-                Don&apos;t have an account ?<span onClick={() => setShowRegister(true)}>Register Now</span>
-              </p>
-            </div>
+            <LoginForm setShowRegister={setShowRegister} />
           )}
         </div>
       </div>
