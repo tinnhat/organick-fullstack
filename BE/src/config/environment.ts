9f5d68ebb68c55112f071f24e83eef3d767def66
Nhat Tin <tinnhat0412@gmail.com>
2024-03-17 15:52:15 +0700

    complete API for user ( not include send email)

--- BE/src/config/environment.ts
+++ BE/src/config/environment.ts
@@ -5,5 +5,9 @@ export const env = {
   DATABASE_NAME: process.env.DATABASE_NAME,
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT,
-  BUILD_MODE: process.env.BUILD_MODE
+  BUILD_MODE: process.env.BUILD_MODE,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
 }
