fedd4186b7ceff241420976c19e9e47e5b9ef9b0
Nhat Tin <tinnhat0412@gmail.com>
2024-03-19 19:38:12 +0700

    update for order api

--- BE/src/config/mongodb.ts
+++ BE/src/config/mongodb.ts
@@ -2,7 +2,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb'
 import { env } from './environment'
 
 // khoi tao mot doi tuong trelloDB ban dau la null
-let trelloDatabaseInstance: any = null
+let databaseInstance: any = null
 
 const mongoClientInstance = new MongoClient(env.MONGODB_URI!, {
   // co the dung chi dinh server api version hoac khong van ket noi dc
@@ -19,14 +19,14 @@ const mongoClientInstance = new MongoClient(env.MONGODB_URI!, {
 export const connectToDatabase = async () => {
   // Connect the client to the server
   await mongoClientInstance.connect()
-  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
-  return trelloDatabaseInstance
+  databaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+  return databaseInstance
 }
 
 // chi goi duoc ham getDB khi da ket noi thanh cong instance toi mongoDB
 export const getDB = () => {
   if (!mongoClientInstance) throw new Error('DB is not connected')
-  return trelloDatabaseInstance
+  return databaseInstance
 }
 
 export const closeDB = async () => {
