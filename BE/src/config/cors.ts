56434441aeaf86a725f81492d8b4971fdbed20f6
Nhat Tin <tinnhat0412@gmail.com>
2024-04-04 11:22:53 +0700

    test cors deloy

--- BE/src/config/cors.ts
+++ BE/src/config/cors.ts
@@ -4,31 +4,12 @@ import { WHITE_LIST } from '../utils/constants'
 import { env } from './environment'
 import { StatusCodes } from 'http-status-codes'
 
-export const corsOptions: any = {
-  origin: function (origin: any, callback: any) {
-    console.log('origin', origin);
-    
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
-      console.log('Origin not provided, allowing in development mode.');
-      return callback(null, true)
-    }
-
-    // Kiểm tra dem origin có phải là domain được chấp nhận hay không
-    if (WHITE_LIST.includes(origin)) {
-      console.log('Origin allowed:', origin);
-      return callback(null, true)
-    }
-
-    // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    console.log('Origin not allowed:', origin);
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
-  },
-
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  optionsSuccessStatus: 200,
-
-  // CORS sẽ cho phép nhận cookies từ request,
-  credentials: true
+export const corsOptions: any = function (req, callback) {
+  let corsOptions
+  if (WHITE_LIST.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
--- BE/src/utils/constants.ts
+++ BE/src/utils/constants.ts
@@ -1,8 +1,4 @@
-export const WHITE_LIST = [
-  'http://localhost:3000',
-  'https://organick-fullstack.vercel.app',
-  'https://organick-fullstack-api.onrender.com'
-]
+export const WHITE_LIST = ['http://localhost:3000', 'https://organick-fullstack.vercel.app']
 export const JWT = {
   expiresIn: '1d',
   expiresInRefresh: '3d'
